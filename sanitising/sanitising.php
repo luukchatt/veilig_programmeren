1b92b6f3d0b7cd26c397fa70cf9090b6a261fa17
HjalmarSnoep <github@snoep.at>
2017-10-03 12:18:09 +0200

    fixed initial bugs.

--- sanitising/sanitising.php
+++ sanitising/sanitising.php
@@ -1,19 +1,22 @@
 <?php
 
-
-if(isset($_REQUEST["email"])
+echo "<h1>Sanitised variables: </h1>";
+/* Start code by Hjalmar Snoep */
+if(isset($_REQUEST["email"]))
 {
 	$email = filter_var($_REQUEST["email"], FILTER_SANITIZE_EMAIL);
 	$email=substr($email,0,50);// no longer than 50.
-	echo "EMAIL: ".$email."<br>"; 
+	echo "EMAIL: '".$email."'<br>"; 
+}else
+{
+	echo "EMAIL: '' (not set)<br>";
 }
+/* End of code by Hjalmar Snoep */
 
-   /*Dalí*/
-if(isset($_REQUEST["rekeningnummer"]))
-   {
-	 $rekeningnummer = filter_var($_REQUEST["rekeningnummer"], FILTER_SANITIZE_EMAIL);
-	 $rekeningnummer = filter_var($_REQUIST["rekeningnummer"], FILTER_FLAG_STRIP_LOW,);
-	 $rekeningnummer 0 =substr($rekeningnummer,0,20);// no longer than 20.
-	 echo "Rekeningnummer: " . $rekeningnummer . "<br>";
-   }
+/*...
+	Jouw code toevoegen, zoals hierboven, a.u.b..
+*/
+
+echo "<a href='form.html'>back</a>";
+
 ?>
